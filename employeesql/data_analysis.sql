7a48255a0046cb2470e2dcd0f87596eb4ce1412c
FLtoCOTransplant <67385063+FLtoCOTransplant@users.noreply.github.com>
2020-11-16 01:09:42 -0700

    lets see who works there

--- employeesql/data_analysis.sql
+++ employeesql/data_analysis.sql
@@ -34,8 +34,20 @@ ON employees.emp_no=dept_manager.emp_no
 INNER JOIN departments 
 ON dept_manager.dept_no = departments.dept_no;
 
--- 5. Hercules, like Dr. Doolitle?
+-- 5. Hercules, like Dr. Doolitle?  Who is named Hercules and has a 
+--    last name that begins with "B"?
 SELECT employees.first_name, 
 	   employees.last_name, 
 	   employees.sex
 FROM employees WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
+
+
+-- 6. Sales, where the money is made!  Who works there?
+SELECT employees.emp_no, 
+	   employees.last_name, 
+	   employees.first_name, 
+	   departments.dept_name
+FROM employees INNER JOIN dept_emp
+ON dept_emp.emp_no=employees.emp_no
+INNER JOIN departments
+ON departments.dept_no=dept_emp.dept_no WHERE dept_name = 'Sales';
