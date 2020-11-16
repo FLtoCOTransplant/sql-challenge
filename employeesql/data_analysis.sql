d0427eaa594a27aef47bb51d48fa8ac1e100192b
FLtoCOTransplant <67385063+FLtoCOTransplant@users.noreply.github.com>
2020-11-16 00:38:18 -0700

    I think I isolated 1986, depends on if it includes or precludes

--- employeesql/data_analysis.sql
+++ employeesql/data_analysis.sql
@@ -4,3 +4,9 @@
 SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
 FROM employees JOIN salaries
 on employees.emp_no=salaries.emp_no;
+
+-- 2. Who was hired in 1986?
+SELECT employees.first_name, employees.last_name, employees.hire_date
+FROM employees WHERE hire_date IN
+	(SELECT hire_date FROM employees
+	WHERE hire_date BETWEEN '1985-12-31' and '1987-01-01');
