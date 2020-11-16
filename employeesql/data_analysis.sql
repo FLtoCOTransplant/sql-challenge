d427e9de7fdf63330b5e8e060556163bd472e9d3
FLtoCOTransplant <67385063+FLtoCOTransplant@users.noreply.github.com>
2020-11-16 00:55:46 -0700

    Hercules!

--- employeesql/data_analysis.sql
+++ employeesql/data_analysis.sql
@@ -29,7 +29,13 @@ SELECT employees.emp_no,
 	   employees.last_name, 
 	   employees.first_name,
 	   departments.dept_name
-FROM employees INNER JOIN dept_manager ON
-employees.emp_no=dept_manager.emp_no
-INNER JOIN departments ON
-dept_manager.dept_no = departments.dept_no;
+FROM employees INNER JOIN dept_manager 
+ON employees.emp_no=dept_manager.emp_no
+INNER JOIN departments 
+ON dept_manager.dept_no = departments.dept_no;
+
+-- 5. Hercules, like Dr. Doolitle?
+SELECT employees.first_name, 
+	   employees.last_name, 
+	   employees.sex
+FROM employees WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
